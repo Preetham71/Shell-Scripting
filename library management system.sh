e7b9d2f7abadd7b878c9ace364e80b70c8d3ae08
Preetham Reddy <80750328+Preetham71@users.noreply.github.com>
2023-05-11 21:05:14 +0530

    Add files via upload

---
+++ library management system.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Prompt user for input
+echo "Please enter the name of the library: "
+read libraryName
+
+# Create the library
+mkdir $libraryName
+
+# Create the subdirectories
+mkdir $libraryName/books $libraryName/students
+
+# Create the necessary files
+touch $libraryName/books/bookList.txt
+touch $libraryName/students/studentList.txt
+
+echo "Your library $libraryName has been created!"
